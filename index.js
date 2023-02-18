4c12cda245f0107b21d689114845187b0a7eeeb0
Stevek-Kim <125353774+Stevek-Kim@users.noreply.github.com>
2023-02-19 09:39:30 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ $ npm install googleapis
 // import as Obj
 const {google} = require('googleapis');
 
-
 const auth = new google.auth.GoogleAuth({  keyFile: "credentials.json",  scopes: "https://www.googleapis.com/auth/spreadsheets", });
 
 const {google} = require('googleapis');
